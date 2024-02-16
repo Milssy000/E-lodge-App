a1c56ec6b9c01d2ebd31e545fa7d626dc6ac167b
Milssy000 <tressymilaure240@gmail.com>
2024-02-16 16:19:00 +0200

    update angular version

--- E-lodgeBurundiApp/src/app/core/services/api.service.ts
+++ E-lodgeBurundiApp/src/app/core/services/api.service.ts
@@ -1,9 +1,84 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { environment } from 'src/app/environments/environment';
+
+import { catchError} from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ApiService {
+    headers = new HttpHeaders()
+        .set('Content-Type', 'application/json')
+        .append('Accept', 'application/json');
+    constructor(private http: HttpClient) {}
 
-  constructor() { }
+    private formatErrors(error: any) {
+        return throwError(error.error);
+    }
+
+    get(
+        path: string,
+      
+        params: HttpParams = new HttpParams(),
+        headers: HttpHeaders = new HttpHeaders()
+    ): Observable<any> {
+
+
+        if (headers['lazyUpdate']) {
+            this.headers = headers;
+        }
+
+        return this.http
+            .get(`${environment.apiUrl}${path}`, {
+                params: params,
+                headers: this.headers,
+            })
+            .pipe(
+
+                catchError(this.formatErrors)
+            );
+    }
+
+    patch(
+        path: string,
+        body: object = {},
+        headers: HttpHeaders = new HttpHeaders()
+    ): Observable<any> {
+        if (headers['lazyUpdate']) {
+            this.headers = headers;
+        }
+
+        return this.http
+            .patch(`${environment.apiUrl}${path}`, JSON.stringify(body), {
+                headers: this.headers,
+            })
+            .pipe(catchError(this.formatErrors));
+    }
+
+    post(
+        path: string,
+        body: object = {},
+        headers: HttpHeaders = new HttpHeaders()
+    ): Observable<any> {
+        if (headers['lazyUpdate']) {
+            this.headers = headers;
+        }
+
+        return this.http
+            .post(`${environment.apiUrl}${path}`, JSON.stringify(body), {
+                headers: this.headers,
+            })
+            .pipe(catchError(this.formatErrors));
+    }
+
+    delete(path: string): Observable<any> {
+        return this.http
+            .delete(`${environment.apiUrl}${path}`)
+            .pipe(catchError(this.formatErrors));
+    }
 }
+
+
+
